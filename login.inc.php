acf552b52d3b5c57436c32fc438a11227072551f
Yaoting Li <87930183+REN2O@users.noreply.github.com>
2021-09-14 20:56:44 +1000

    Update login.inc.php

--- login.inc.php
+++ login.inc.php
@@ -9,13 +9,13 @@ if (isset($_POST["submit"])) {
 	require_once 'functions.inc.php';
 
 	if (emptyInputLogin($username, $pwd) !== false) {
-		header("location: ../login.php?error=emptyinput");
+		header("location: login.php?error=emptyinput");
 		exit();
 	}
 	
 	loginUser($conn, $username, $pwd);
 }
 else{
-	header("location: ../logIn.php");
+	header("location: logIn.php");
 	exit();
 }
